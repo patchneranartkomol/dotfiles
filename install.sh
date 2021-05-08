6b9d9e46c9cd40a174f29985f48137c8362a474e
Patch Neranartkomol <patcharaponn@gmail.com>
2021-05-08 16:32:10 -0700

    Add i3 Config

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -sf "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
 
 rm -rf "$HOME/.config/X11"
 ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
