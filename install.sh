558aff61cbccf655918fa155d4e7700b282dc89a
Patch Neranartkomol <patcharaponn@gmail.com>
2021-05-08 20:20:31 -0700

    Add Redshift (blue blocker script) conf

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ ln -sf "$HOME/dotfiles/X11" "$HOME/.config"
 
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+ln -s "$HOME/dofiles/redshift/redshift.conf" "$HOME/.config/redshift.conf"
