057ae85f18d8b4518450329c2f44c93f62bcf7ea
Patch Neranartkomol <patcharaponn@gmail.com>
2021-05-25 16:22:16 -0700

    Add tmux config

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
 
 rm -rf "$XDG_CONFIG_HOME/X11"
@@ -8,7 +7,7 @@ ln -sf "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
-ln -sf "$HOME/dofiles/redshift/redshift.conf" "$XDG_CONFIG_HOME/redshift.conf"
+ln -sf "$DOTFILES/redshift/redshift.conf" "$XDG_CONFIG_HOME/redshift.conf"
 
 mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
@@ -16,3 +15,5 @@ ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
