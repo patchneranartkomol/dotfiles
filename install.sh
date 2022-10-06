984bc5b231878e48f452982b886275d578cb87e0
Patch Neranartkomol <patch@neranartkomol.com>
2022-10-06 14:05:59 -0700

    Add nvim to install shell script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
+ln -s "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -sf "$DOTFILES/X11" "$XDG_CONFIG_HOME"
